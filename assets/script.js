f0ecc883cabb9cacc74e400ed816f537073ce443
Velma <k.ceb.09@gmail.com>
2020-10-13 23:46:52 -0400

    Added questions to JS and put files into a folder

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,36 @@
+// Global Questions
+var questions = [
+  {
+    question: "Which of the following are data types?",
+    choices: ["strings", "numbers", "arrays", "all of the above"],
+    answer: "all of the above"
+  }
+  {
+    question: "How is JavaScript interpretted?",
+    choices: ["Server", "Object", "Client", "None of the Above"],
+    answer: "Client"
+  }
+  {
+    question: "Which operator is used to assign a variable?",
+    choices: ["+" , "=", "-", "*"],
+    answer: "="
+  }
+  {
+    question: "What was JavaScript formally known as?",
+    choices: ["Mocha", "Hot Coffee", "Scribing", "Eich"],
+    answer: "Mocha"
+  }
+  {
+    question:"What does a for...loop repeat over?"
+    choices: ["property names", ""]
+    answer: 
+  }
+]
+
+
+
+
+
 
 var startBtn = document.querySelector("#start")
 // Set timer for 75 seconds 
@@ -22,14 +55,18 @@ function setTime() {
 setTime();
 
 // Set timer to start on click
-// startBtn.addEventListener("click", function() {
-//     count++;
-//     setCounterText();
-//   });
+startBtn.addEventListener("click", function() { 
+    secondsLeft++;
+    setCounterText();
+  });
   
-//   startBtn.addEventListener("click", function() {
-//     if(count > 0) {
-//       count--;
-//       setCounterText();
-//     }
-//   }); 
+  startBtn.addEventListener("click", function() {
+    if(secondsLeft > 0) {
+      secondsLeft--;
+      setCounterText();
+    }
+  }); 
+
+//  create a start button
+// start timer upon click
+// 
